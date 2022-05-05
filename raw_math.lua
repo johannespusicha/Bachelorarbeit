8ec47b8dc8a7c32fbe2b5f4c76a073746ac310d2
johannespusicha <j.pusicha@outlook.de>
2022-05-05 15:11:29 +0200

    auto backup on Johannes MacBook Pro: 05.05.2022 15:11:27 - 1 were changed
    Affected files:
    raw_math.lua

--- raw_math.lua
+++ raw_math.lua
@@ -1,5 +1,5 @@
 if FORMAT:match 'latex' then
-	function Para (elem)
-		return pandoc.read('Das ist ein Text' .. elem.content, 'markdown').blocks
+	function Math (elem)
+		return pandoc.RawInline('latex', elem.text)
 	end
 end
